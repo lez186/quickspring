5cb339fe2928e61b245447d78f1d00fc0726fab3
lez186 <258508474@qq.com>
2020-07-19 11:27:31 +0800

    初始版本

---
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.example.mapper;
+
+import com.example.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    public List<User> queryUserList();
+}
